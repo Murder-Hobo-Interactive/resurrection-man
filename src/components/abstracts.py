5eb7da562a63ca029e9dcfe1d90395e57648fe19
Chris Palmer <capalmer1013@gmail.com>
2023-01-25 19:58:00 -0500

    forgot the file

---
+++ src/components/abstracts.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class Base(ABC):
+    # This is just here to be a base class
+    pass
+
+class AbstractActor(Base):
+    @abstractmethod
+    def __init__(self, controller)->None:  # type: ignore
+        pass
+
+    @abstractmethod
+    def move(self, x: int, y: int)->None:
+        pass
+
+    @abstractmethod
+    def update(self)->None:
+        pass
+
+    @abstractmethod
+    def draw(self)->None:
+        pass
+
+class AbstractController(Base):
+    @abstractmethod
+    def register(self, actor)->None: # type: ignore
+        pass
